fc7fd8aaa16e11b9c1f9bbaacf69f181324a664e
Omar Naman <onaman@uwaterloo.ca>
2021-08-26 21:48:15 -0400

    add CreateGroup cloud function

--- firebase_functions/functions/src/index.ts
+++ firebase_functions/functions/src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 admin.initializeApp();
@@ -21,4 +22,38 @@ export const SignUp = functions.auth.user().onCreate((user) => {
     last_group: groupDocumentRef,
     group_ref_array: [groupDocumentRef]});
   itemsDocumentRef.create({});
+});
+
+
+export const CreateGroup = functions.https.onCall((data, context) => {
+  if (context == null) {
+    return;
+  }
+  if (context.auth == null) {
+    return;
+  }
+  const userId = context.auth?.uid;
+  const groupName = data.groupName;
+  const groupDocumentRef =
+    admin.firestore().collection("groups").doc();
+  const userDocumentRef =
+    admin.firestore().collection("users").doc(userId);
+  groupDocumentRef.create({
+    owner: userId,
+    name: groupName,
+  });
+  return userDocumentRef.get().then((doc) => {
+    let groups = doc.data()?.group_ref_array || null;
+    if (groups == null) {
+      groups = [groupName];
+    } else {
+      groups = groups.concat(groupDocumentRef);
+    }
+    userDocumentRef.update({
+      group_ref_array: groups,
+    });
+    return {text: "Done", id: groupDocumentRef};
+  }).catch((err) => {
+    return {error: err};
+  });
 });
